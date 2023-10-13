ef0d37be6817e2de54f7355b5b6d80f4f4274adf
reddychavva <140389661+reddychavva@users.noreply.github.com>
2023-10-13 11:57:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TV from './components/TV';
+import AC from './components/AC';
+
+function App() {
+  return (
+<div>
+<h1>Electronics Shop</h1>
+<TV />
+<AC />
+</div>
+  );
+}
+
+export default App;
+
